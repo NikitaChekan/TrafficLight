306432fd1741c2b7b348911fda4e695ca6592ef5
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-09-16 00:16:19 +0300

    Added rounding view

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,9 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redCircleView: UIView!
+    @IBOutlet var yellowCircleView: UIView!
+    @IBOutlet var greenCircleView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redCircleView.layer.cornerRadius = 65
+        yellowCircleView.layer.cornerRadius = 65
+        greenCircleView.layer.cornerRadius = 65
+        
     }
 
 
