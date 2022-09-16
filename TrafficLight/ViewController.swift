8f6c6d324674d2a88b8d811bd1605583b870563a
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-09-17 00:30:51 +0300

    Implemented the logic of the button

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     @IBOutlet var yellowCircleView: UIView!
     @IBOutlet var greenCircleView: UIView!
     
+    @IBOutlet var changeColorButton: UIButton!
+    
+    private var changeColor = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,8 +24,36 @@ class ViewController: UIViewController {
         yellowCircleView.layer.cornerRadius = 65
         greenCircleView.layer.cornerRadius = 65
         
+        changeColorButton.layer.cornerRadius = 10
+        
+        redCircleView.alpha = 0.3
+        yellowCircleView.alpha = 0.3
+        greenCircleView.alpha = 0.3
     }
 
-
+    @IBAction func changeColorButtonDidTapped() {
+        changeColorButton.setTitle("NEXT", for: .normal)
+        
+        switch changeColor {
+        case 0:
+            redCircleView.alpha = 1
+            changeColor += 1
+        case 1:
+            redCircleView.alpha = 0.3
+            yellowCircleView.alpha = 1
+            changeColor += 1
+        case 2:
+            redCircleView.alpha = 0.3
+            yellowCircleView.alpha = 0.3
+            greenCircleView.alpha = 1
+            changeColor += 1
+        case 3:
+            redCircleView.alpha = 1
+            yellowCircleView.alpha = 0.3
+            greenCircleView.alpha = 0.3
+            changeColor = 1
+        default:
+            break
+        }
+    }
 }
-
